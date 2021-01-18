77c0700b869d256a2fe9255d46073d024ec3cb01
DRIOWYA Abdel <abdodriowya@gmail.com>
2021-01-18 23:21:10 +0100

    hopefully more sustainable

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,8 @@ import * as puppeteer from 'puppeteer';
   const page = await browser.newPage();
 
   await page.goto('https://www.ebay.co.uk/', { waitUntil: 'networkidle0' });
-  await page.click('span#gh-ug-flex>a', { delay: 200 }); // TODO use more generic solution 
+  const registerButton = await getElement(page, 'register', 'a', 'header');
+  await registerButton.click({ delay: 200 });
   await page.waitForSelector('#firstname', { visible: true }); // TODO check if interactive with waitForFunction?
   await page.waitFor(500); // Workaround because visible doesn't imply ready to receive actions :(
   await page.type('#firstname', 'myName', { delay: 200 });
@@ -22,3 +23,20 @@ import * as puppeteer from 'puppeteer';
   console.log('done');
 
 })();
+
+
+async function getElement(page: puppeteer.Page, textToSearchFor: string, selector: string, containerSelector = 'body') {
+  const container = await page.$(containerSelector);
+  const links = await container.$$(selector);
+  const registerButton = await findAsync(links, async (link) => {
+    const textContent = (await (await link.getProperty('textContent')).jsonValue());
+    return textContent === textToSearchFor;
+  });
+  return registerButton;
+}
+
+async function findAsync<T>(array: T[], predicate: (value: T, index?: number, obj?: T[]) => Promise<boolean>): Promise<T> {
+  const candidates = await Promise.all(array.map(predicate));
+  const index = candidates.findIndex(candidate => candidate);
+  return array[index];
+}
