c47285a8985d5099e49d53dfdc1ba6cc01f84be4
DRIOWYA Abdel <abdodriowya@gmail.com>
2021-01-19 08:27:50 +0100

    check if page is interactive before start typing

--- src/index.ts
+++ src/index.ts
@@ -9,16 +9,15 @@ import * as puppeteer from 'puppeteer';
   await page.goto('https://www.ebay.co.uk/', { waitUntil: 'networkidle0' });
   const registerButton = await getElement(page, 'register', 'a', 'header');
   await registerButton.click({ delay: 200 });
-  await page.waitForSelector('#firstname', { visible: true }); // TODO check if interactive with waitForFunction?
-  await page.waitFor(500); // Workaround because visible doesn't imply ready to receive actions :(
+  await page.waitForSelector('#firstname', { visible: true });
+  await page.waitForFunction(() => document.readyState === 'complete');
   await page.type('#firstname', 'myName', { delay: 200 });
   await page.type('#lastname', 'myLastName', { delay: 200 });
-  await page.type('#Email', 'pffzadaz@outlook.com', { delay: 200 });
+  await page.type('#Email', 'pff2z221az@outlook.com', { delay: 200 });
   await page.type('#password', 'FZFZUHZFUZ273', { delay: 200 });
   await page.waitForFunction(() => {
     return document.querySelectorAll('button#EMAIL_REG_FORM_SUBMIT:not([disabled])').length > 0
   });
-  // console.log(await page.waitForFunction('button#EMAIL_REG_FORM_SUBMIT:not([disabled])'));
   await page.click('button#EMAIL_REG_FORM_SUBMIT', { delay: 200 });
   console.log('done');
 
