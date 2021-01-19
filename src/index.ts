517fc7f999e7c7eb387595e4a772c81bdf067497
DRIOWYA Abdel <abdodriowya@gmail.com>
2021-01-19 22:24:07 +0100

    add some error handling

--- src/index.ts
+++ src/index.ts
@@ -4,29 +4,35 @@ import fetch from 'node-fetch';
 import { generateRandomPassword } from './utils';
 
 (async () => {
-  const namesResponse = await (await fetch('https://namey.muffinlabs.com/name.json?count=1&with_surname=true&frequency=common')).json();
-  if (!namesResponse || !namesResponse.length) {
-    console.error('unable to fetch name');
-  }
-  const [firstName, lastName] = namesResponse[0].split(' ');
-  const browser = await puppeteer.launch({
-    headless: false,
-  });
-  const page = await browser.newPage();
+  try {
+    const namesResponse = await (await fetch('https://namey.muffinlabs.com/name.json?count=1&with_surname=true&frequency=common')).json();
+    if (!namesResponse || !namesResponse.length) {
+      console.error('unable to fetch name');
+    }
+    const [firstName, lastName] = namesResponse[0].split(' ');
+    const browser = await puppeteer.launch({
+      headless: false,
+    });
+    const page = await browser.newPage();
 
-  await page.goto('https://www.ebay.co.uk/', { waitUntil: 'networkidle0' });
-  const registerButton = await getElement(page, 'register', 'a', 'header');
-  await registerButton.click({ delay: 200 });
-  await page.waitForSelector('#firstname', { visible: true });
-  await page.waitForFunction(() => document.readyState === 'complete');
-  await page.type('#firstname', firstName, { delay: 200 });
-  await page.type('#lastname', lastName, { delay: 200 });
-  await page.type('#Email', `${firstName}${lastName}@outlook.com`, { delay: 200 });
-  await page.type('#password', generateRandomPassword(), { delay: 200 });
-  await page.waitForFunction(() => {
-    return document.querySelectorAll('button#EMAIL_REG_FORM_SUBMIT:not([disabled])').length > 0
-  });
-  await page.click('button#EMAIL_REG_FORM_SUBMIT', { delay: 200 });
-  console.log('done');
+    await page.goto('https://www.ebay.co.uk/', { waitUntil: 'networkidle0' });
+    const registerButton = await getElement(page, 'register', 'a', 'header');
+    await registerButton.click({ delay: 200 });
+    await page.waitForSelector('#firstname', { visible: true });
+    await page.waitForFunction(() => document.readyState === 'complete');
+    await page.type('#firstname', firstName, { delay: 200 });
+    await page.type('#lastname', lastName, { delay: 200 });
+    await page.type('#Email', `${firstName}${lastName}@outlook.com`, { delay: 200 });
+    await page.type('#password', generateRandomPassword(), { delay: 200 });
+    await page.waitForFunction(() => {
+      return document.querySelectorAll('button#EMAIL_REG_FORM_SUBMIT:not([disabled])').length > 0
+    });
+    await page.click('button#EMAIL_REG_FORM_SUBMIT', { delay: 200 });
+    await page.waitForNavigation();
+    console.log('done');
+  } catch (err) {
+    console.error('Something Wrong happened while creating an account');
+    console.error(err);
+  }
 
 })();
